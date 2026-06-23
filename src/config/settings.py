caa1f74fe590b1b1abaf6a85291fd919a2b96189
Enzo Cardoso Martins <enzocm.26@gmail.com>
2026-06-23 20:22:37 -0300

    configura pytest.ini e banco sqlite (apenas para eu conseguir passar meus testes. irei usar o que o restante do grupo criar mais tarde)

--- src/config/settings.py
+++ src/config/settings.py
@@ -6,3 +6,30 @@ Competência: Registra os aplicativos criados (usuarios, catalogo, mercados),
 configura a conexão com o banco de dados SQLite, define variáveis de 
 ambiente, sistema de templates e middlewares de segurança.
 """
+from pathlib import Path
+
+# Caminho base do projeto (necessário para o SQLite saber onde criar o arquivo)
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Configurações básicas obrigatórias para a execução do Django
+SECRET_KEY = "django-insecure-chave-de-teste-para-o-ambiente-de-tp2"
+DEBUG = True
+
+INSTALLED_APPS = [
+    # Apps nativos essenciais para autenticação, testes e tabelas base
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    
+    # Apps do projeto
+    "users",
+    "catalog",
+    "markets",
+]
+
+# Configuração do Banco de Dados SQLite (Definido na especificação do grupo)
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
