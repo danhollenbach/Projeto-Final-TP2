bf38bc4484735efec96b0598d1ceb78ace732306
Enzo Cardoso Martins <enzocm.26@gmail.com>
2026-06-23 20:24:00 -0300

    Implementacao: estrutura inicial do modelo Mercado

--- src/markets/models.py
+++ src/markets/models.py
@@ -7,3 +7,15 @@ as compras são realizadas (nome, endereço, coordenadas GPS, status de
 funcionamento).
 Histórias relacionadas: US-24 (Cadastrar mercados) e US-25 (Editar/Remover mercados).
 """
+
+from django.db import models
+
+class Mercado(models.Model):
+    """
+    Define a entidade Mercado no banco de dados.
+    """
+    nome = models.CharField(max_length=150)
+    endereco = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return str(self.nome)
