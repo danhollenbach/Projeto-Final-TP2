b2ddb8edc7a37c9c68ef7d5b9a9c4053ace49b40
ekenjimatsushita <dudukcortez@gmail.com>
2026-07-01 10:18:17 -0300

    feat: adicionar o modulo de servicos

---
+++ src/catalog/services.py
@@ -0,0 +1,10 @@
+"""
+Módulo: Serviços do catálogo
+
+Resumo:
+Centraliza as regras de negócio relacionadas ao gerenciamento do catálogo de
+produtos, incluindo operações de identificação, validação e unificação de
+produtos duplicados.
+
+"""
+
